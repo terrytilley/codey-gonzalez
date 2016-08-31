2f02960cacf547ce5a77467cd7dfcc8670549235
Michael <mjosephmiller.uk@gmail.com>
2016-08-31 22:58:30 +0100

    adds a game test to expect a JS function is displayed on index

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,7 +1,17 @@
-# require 'rails_helper'
-#
-#   feature 'user can see their WPM at the end of a game' do
-#
-#   end
-#
-# end
+require 'rails_helper'
+
+feature "a game can be played" do
+
+  context "on page load code should be displayed" do
+    it "should display a JS function to copy" do
+      visit('/')
+      page.find("#code")
+      expect(page).to have_selector("pre")
+    end
+
+  # context 'the last keystroke ends the game' do
+  #   it 'should call endGame() once the last character is matched' do
+  #
+  #   end
+  end
+end
