ae9d7962b457f05f938f2f4faa7db60cfd447377
sus111 <sgraham535@gmail.com>
2016-09-01 18:07:27 +0100

    tests

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -1,4 +1,21 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe 'Code', type: :model do
+describe Code do
+
+  describe '.difficulty' do
+
+    it "returns 'Easy' if code level is less than or equal to two" do
+      expect(subject.difficulty(2)).to eq 'Easy'
+    end
+
+    it "returns 'Medium' if code level is less equal to 3" do
+      expect(subject.difficulty(3)).to eq 'Medium'
+    end
+
+    it "returns 'Hard' if code level is greater than 3" do
+      expect(subject.difficulty(5)).to eq 'Hard'
+    end
+
+  end
+
 end
