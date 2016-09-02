74f16a8ea023d3013fe08fcbe72c3056a4851127
Mali Michael <malimccalla@gmail.com>
2016-09-02 21:18:12 +0100

    adds tab ability

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -14,11 +14,22 @@ $(document).on("turbolinks:load", (function(){
   })();
 
   $(document).on("keypress", function( event ) {
+    console.log(event.keyCode);
     Game.start(event.keyCode);
   });
 
   $(document).one("keypress", function( event ){
     Timer.startTimer();
+  });
+
+  $(document).on('keydown', function(event) {
+   if (event.keyCode == 9) {
+     event.preventDefault();
+     var e = $.Event('keypress');
+     e.keyCode = 32;
+     $(document).trigger(e);
+     $(document).trigger(e);
+    }
   });
 
 }));
