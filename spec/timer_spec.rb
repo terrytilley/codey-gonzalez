b9792cc097e6731dbd2f5e0995ae15b5278a1380
sus111 <sgraham535@gmail.com>
2016-09-02 11:27:26 +0100

    start game test

--- spec/timer_spec.rb
+++ spec/timer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'a timed game can be played' do
 
   context 'a timer is run' do
-    it "starts when the first key is pressed", js_errors: false, js: true do
+    it "starts when the first key is pressed", js: true do
       visit('/')
       find("body").native.send_key("h")
       expect(page).to have_css('div.timerStarted')
